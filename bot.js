5bd25bb50a77b7b780e6940fd12cafbf8a0fe3c8
CuzImM4S <49302901+CuzImM4S@users.noreply.github.com>
2019-06-12 18:51:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "-";
+var prefix = ".";
 
 client.on('ready', () => {
 	console.log('Dream iS Here');
@@ -21,20 +21,20 @@ client.on('ready', () => {
     client.on('message', message => {
         var dream = message.content.split(` `).slice(1).join(' ');
           if (!dreamv2.includes(message.author.id)) return;
-          if(message.content.startsWith(prefix + "ply")){
+          if(message.content.startsWith(prefix . "ply")){
             client.user.setActivity(dream);
             message.channel.send(`✔️ **Done Playing Now __${dream}__**`)
         }
-          if (message.content.startsWith(prefix + 'wt')) {
+          if (message.content.startsWith(prefix . 'wt')) {
       client.user.setActivity(dream, {type:'WATCHING'});
           message.channel.send(`✔️  **Done Watching Now __${dream}__ **`)
       } else
-      if (message.content.startsWith(prefix + 'ls')) {
+      if (message.content.startsWith(prefix . 'ls')) {
       client.user.setActivity(dream , {type:'LISTENING'});
           message.channel.send(`✔️ **Done Listening Now __${dream}__** `)
       } else
-      if (message.content.startsWith(prefix + 'st')) {
-    client.user.setGame(dream, "https://www.twitch.tv/dream");
+      if (message.content.startsWith(prefix . 'st')) {
+    client.user.setGame(dream, "https://www.twitch.tv/M4S");
          message.channel.send(`✔️ **Done Stream Now  __${dream}__ **`)
       }
  
